4bb191268d8c6c2947be456a4c7ddbf2aaefe275
carbha01234 <charderajnee20@gmail.com>
2024-11-01 00:17:50 +0530

    all files are added

---
+++ config.py
@@ -0,0 +1,3 @@
+MODLE_FILE_PATH = r'C:\Class_Notes\Deloitte_Task\Delloite_Practice\ML_MODEL\Classification\Loan____\Project_app\Loan.pkl'
+
+JSON_FILE_PATH  = r'C:\Class_Notes\Deloitte_Task\Delloite_Practice\ML_MODEL\Classification\Loan____\Project_app\Loan.json'
